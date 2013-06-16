3c8863fd53b2f15ab5ae9792ae8b20d8304a97e1
Yury Chuyko <mrgrey@yandex-team.ru>
2013-06-16 23:29:32 +0400

    added MarkCalculator interface

---
+++ src/java/ru/ifmo/examtool/model/MarkCalculator.java
@@ -0,0 +1,17 @@
+package ru.ifmo.examtool.model;
+
+import java.util.Collection;
+
+/**
+ * Author: Yury Chuyko
+ * Date: 16.06.13
+ */
+public interface MarkCalculator {
+
+    Mark calculate(Collection<Boolean> questionAnswerResults);
+
+    enum Mark {
+        _2, _3, _4, _5
+    }
+
+}
