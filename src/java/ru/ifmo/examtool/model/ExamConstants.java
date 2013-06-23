0c1b729e4ce8e80c28fa2748064dc2afc0d9f39d
Yury Chuyko <mrgrey@yandex-team.ru>
2013-06-24 00:53:43 +0400

    constants class introduced

---
+++ src/java/ru/ifmo/examtool/model/ExamConstants.java
@@ -0,0 +1,13 @@
+package ru.ifmo.examtool.model;
+
+/**
+ * Author: Yury Chuyko mrgrey@yandex-team.ru
+ * Date: 24.06.13
+ */
+public final class ExamConstants {
+
+    public static final int MAX_QUESTION_QUEUE_SIZE = 20;
+
+    private ExamConstants() {
+    }
+}
